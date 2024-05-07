5ef410cf38f4b2de75c261f5a3370cefb35c9013
lxgr-linux <lxgr@protonmail.com>
2024-05-07 13:29:58 +0200

    Unified codegen

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,12 +1,6 @@
-rm -rf ./Buf ./Clients
+rm -rf ./Buf
 cd ./cosmos-sdk/proto
 
-buf generate --template ../../buf.gen.types.yaml --include-imports
-buf generate --template ../../buf.gen.client.yaml $(find | grep /tx.proto | awk '{print "--path="$1}' | xargs)
-paths=$(find | grep -E "/query.proto|/service.proto")
-echo $paths
-for path in $paths; do
-    p="../../Buf$(python -c "print('/'.join(i.capitalize() for i in \"$(echo $path | sed "s/query.proto//g" | sed "s/service.proto//g" | sed 's/\.//g')\".split('/')))")"
-    buf generate --template ../../buf.gen.grpc.yaml -o $p $path
-    echo $p
-done
+buf generate --template ../../buf.gen.yaml --include-imports
+
+rm $(find ../../Buf | grep "TxGrpc.pb.cs")
